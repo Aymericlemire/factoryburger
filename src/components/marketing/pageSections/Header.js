22cc7ddcdc6990156f31c3716cbad664cc855719
Aymericlemire <lemire.aymericpro@gmail.com>
2026-04-23 21:06:08 +0200

    Add Header component and Button element for improved UI

--- src/components/marketing/pageSections/Header.js
+++ src/components/marketing/pageSections/Header.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import Logo from '../../../media/image/svg/Logo-burger-house.svg'
 import Icone from '../../../media/image/svg/ico-bag-clickAndCollect.svg'
+import Button from './elements/Button'
 
 export default function Header() {
   return (
     <div className='flex items-center justify-between py-10'>
-        <div className='bg-red-500 h-20 w-full py-2 px-8 '>
+        <div className='h-20 w-full py-2 px-8 '>
             <img src={Logo} alt="Burger House app" className='w-64'/>
         </div>
 
         <div className='w-full text-secondary flex items-center justify-end pr-8'>
-            <img src={Icone} alt="Click&Connect" className='w-5 h-5 mr-2 ' />
+            <div>
+                <img src={Icone} alt="Click&Connect" className='w-5 h-5 mr-2 ' />
                 <span>Commandez votre repas en ligne</span>
+            </div>
+
+            <div>
+                <Button />
+            </div>
+
         </div>
-        
+
     </div>
   )
 }
